e4d5de4167b50e2a2be9cbad99d84838d85d6725
Giss Urgiles <giizz.dillon@hotmail.com>
2022-03-02 20:48:45 -0500

    Cuarta Parte

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+	$host ='localhost';   //el nombre del servidor
+	$user = 'root';       //usuario
+	$password = '';       //contraseña
+	$db = 'facturacion';  //nombre de nuestra base de datos
+
+	//conexion
+
+	$conection = @mysqli_connect($host, $user, $password, $db);
+
+	if(!$conection){
+		echo "Error en la conexión";
+	}
+	
+ ?>
---
+++ sistema/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<?php include "include/scripts.php"; ?>
+	<title>Sistema de Ventas</title>
+</head>
+<body>
+	<?php include "include/header.php"; ?>
+	<section id="container">
+		<h1>Sistema Love & Pets</h1>
+	</section>
+	<?php include "include/footer.php"; ?>
+</body>
+</html>
