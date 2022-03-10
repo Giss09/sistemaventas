010aed1d07d76b3fb3a9dc89d407c924824d3989
Giss09 <98498457+Giss09@users.noreply.github.com>
2022-03-09 21:08:06 -0500

    Modificación de los Scripts por Gissella

--- sistema/include/scripts.php
+++ sistema/include/scripts.php
@@ -1,3 +1,18 @@
 <link rel="stylesheet" type="text/css" href="css/style.css">
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/icons.js"></script>
+<script type="text/javascript" src="js/all.js"></script>
+<script type="text/javascript" src="js/all.min.js"></script>
+<script type="text/javascript" src="js/brands.min.js"></script>
+<script type="text/javascript" src="js/conflict-detection.js"></script>
+<script type="text/javascript" src="js/conflict-detection.min.js"></script>
+<script type="text/javascript" src="js/fontawesome.js"></script>
+<script type="text/javascript" src="js/fontawesome.min.js"></script>
+<script type="text/javascript" src="js/regular.js"></script>
+<script type="text/javascript" src="js/regular.min.js"></script>
+<script type="text/javascript" src="js/solid.js"></script>
+<script type="text/javascript" src="js/solid.min.js"></script>
+<script type="text/javascript" src="js/v4-shims.js"></script>
+<script type="text/javascript" src="js/v4-shims.min.js"></script>
 <script type="text/javascript" src ="js/functions.js"></script>
 <?php include "functions.php"; ?>
