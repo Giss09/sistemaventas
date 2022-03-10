6b1b56f1158c1a8a808942d485b61d5ef7d84a8f
Giss09 <98498457+Giss09@users.noreply.github.com>
2022-03-09 20:39:53 -0500

    Modificacion del index por Darlyn

--- sistema/index.php
+++ sistema/index.php
@@ -1,14 +1,17 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="UTF-8">	
 	<?php include "include/scripts.php"; ?>
 	<title>Sistema de Ventas</title>
 </head>
 <body>
 	<?php include "include/header.php"; ?>
 	<section id="container">
-		<h1>Sistema Love & Pets</h1>
+		<h2>Sistema Love & Pets</h2>
 	</section>
 	<?php include "include/footer.php"; ?>
 </body>
