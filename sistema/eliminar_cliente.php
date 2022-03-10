c697ea867fed62697aaf9123f2f60b1e90411447
Giss09 <98498457+Giss09@users.noreply.github.com>
2022-03-09 20:51:30 -0500

    Modificación de Eliminar Cliente por Gissella

--- sistema/eliminar_cliente.php
+++ sistema/eliminar_cliente.php
@@ -1,40 +1,39 @@
 <?php  
 session_start();
-if($_SESSION['rol'] != 1 and $_SESSION['rol'] != 4){
+if($_SESSION['rol'] != 1){
 	header("location: ./");
 }
 	include "../conexion.php";
 		if(!empty($_POST)){
-				if(empty($_POST['idcliente'])){
-					header("location: lista_cliente.php");
+				if(empty($_POST['idproveedor'])){
+					header("location: lista_proveedor.php");
 					mysqli_close($conection);
 				}
-				$idcliente = $_POST['idcliente'];
+				$idproveedor = $_POST['idproveedor'];
 				//$query_delete = mysqli_query($conection,"DELETE FROM cliente WHERE idusuario = $idusuario");
-				$query_delete = mysqli_query($conection, "UPDATE cliente SET estatus = 0 WHERE idcliente = $idcliente");
+				$query_delete = mysqli_query($conection, "UPDATE proveedor SET estatus = 0 WHERE codproveedor = $idproveedor");
 				mysqli_close($conection);
 				if($query_delete){
-					header("location: lista_cliente.php");
+					header("location: lista_proveedor.php");
 				}else {
-					echo "Error al eliminar el cliente";
+					echo "Error al eliminar el proveedor";
 				}
 			}
 		
 			if(empty($_REQUEST['id'])){
-				header("location: lista_cliente.php");
+				header("location: lista_proveedor.php");
 				mysqli_close($conection);
 			}else{
-				$idcliente = $_REQUEST['id'];
-				$query = mysqli_query($conection,"SELECT *	FROM cliente WHERE idcliente = $idcliente");
+				$idproveedor = $_REQUEST['id'];
+				$query = mysqli_query($conection,"SELECT *	FROM proveedor WHERE codproveedor = $idproveedor");
 				mysqli_close($conection);
 				$result = mysqli_num_rows($query);
 				if ($result > 0) {
 					while($data = mysqli_fetch_array($query)){
-						$cedula = $data['cedula'];
-						$nombre = $data['nombre'];
+						$proveedor = $data['proveedor'];
 					}
 				}else{
-					header("location: lista_cliente.php");
+					header("location: lista_proveedor.php");
 				}
 			}
 ?>
@@ -43,20 +42,22 @@ if($_SESSION['rol'] != 1 and $_SESSION['rol'] != 4){
 <head>
 	<meta charset="UTF-8">
 	<?php include "include/scripts.php"; ?>
-	<title>Eliminar Cliente</title>
+	<title>Eliminar Proveedor</title>
 </head>
 <body>
 	<?php include "include/header.php"; ?>
 	<section id="container">
 		<div class="data_delete"> 
-			<h2>¿Esta seguro de eliminar al cliente</h2>
+			<i class="fa fa-user-times fa-7x" color= "#88B0A4" aria-hidden="true"></i>
 			<br>
-			<p>Cédula: <span><?php echo $cedula; ?></span></p>
-			<p>Nombre del Cliente: <span><?php echo $nombre; ?></span></p>
+			<br>	
+			<h2>¿Esta seguro de eliminar al Proveedor</h2>
+			<br>
+			<p>Nombre del Proveedor: <span><?php echo $proveedor; ?></span></p>
 			<form method="POST" action="">
-				<input type="hidden" name="idcliente" value="<?php echo $idcliente; ?>">
-				<a href="lista_cliente.php" class="btn_cancel">Cancelar</a>
-				<input type="submit" value="Aceptar" class="btn_ok">
+				<input type="hidden" name="idproveedor" value="<?php echo $idproveedor; ?>">
+				<button type="submit" class="btn_cancel"><i class="fa fa-ban" aria-hidden="true"></i> Cancelar</button>
+				<button type="submit" class="btn_ok"><i class="fa fa-trash" aria-hidden="true"></i> Aceptar</button>
 			</form>
 		</div>
 	</section>
