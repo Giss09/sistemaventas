18dd16d03fa53d9bec5a5bd43d91f6580deee5ad
Giss09 <98498457+Giss09@users.noreply.github.com>
2022-03-09 21:07:50 -0500

    Modificacion del header por Darlyn

--- sistema/include/header.php
+++ sistema/include/header.php
@@ -1,18 +1,15 @@
 <?php 
-	session_start();
-
 		if(empty($_SESSION['active'])){
 					header('location: ../');
 		}
 ?>
 <header>
 		<div class="header">
-			
-			<h1>Sistema de Facturación Love & Pets</h1>
+			<p style="text-align- left" ><i class="fa fa-paw" aria-hidden="true"></i> Sistema de Facturación Love & Pets</p>
 			<div class="optionsBar">
 				<p>Quito, <?php echo fechaC();?></p>
 				<span>|</span>
-				<span class="user"><?php echo $_SESSION['nombre'] ?></span>
+				<span class="user"><?php echo $_SESSION['nombre'].'-'.$_SESSION['rol'];?></span>
 				<img class="photouser" src="img/user.png" alt="Usuario">
 				<a href="salir.php"><img class="close" src="img/salir.png" alt="Salir del sistema" title="Salir"></a>
 			</div>
