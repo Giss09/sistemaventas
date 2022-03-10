759134523c452bd400ffa550b1ca12152f675cde
Giss09 <98498457+Giss09@users.noreply.github.com>
2022-03-09 20:52:24 -0500

    Creacion lista usuario por Darlyn

--- sistema/lista_usuarios.php
+++ sistema/lista_usuarios.php
@@ -1,4 +1,8 @@
 <?php 
+session_start();
+if($_SESSION['rol'] != 1){
+	header("location: ./");
+}
 		include	"../conexion.php";
 
 ?>
@@ -12,11 +16,11 @@
 <body>
 	<?php include "include/header.php"; ?>
 	<section id="container">
-		<h1>Lista de Usuarios</h1>
-			<a href="registro_usuario.php" class="btn_new">Crear Usuario</a>
-			<form class="buscar_usuario.php" method="GET" class="form_search">
+		<h1><i class="fa fa-users fa-lg" aria-hidden="true"></i> Lista de Usuarios</h1>
+			<a href="registro_usuario.php" class="btn_new"><i class="fa fa-user-plus" aria-hidden="true"></i> Crear Usuario</a>
+			<form action="buscar_usuario.php" method="get" class="form_search">
 					<input type="text" name="busqueda" id="busqueda" placeholder="Buscar Usuario">
-					<input type="submit" value="Buscar" class="btn_search">
+					<button type="submit" class="btn_search"><i class="fa fa-search" aria-hidden="true"></i></button>
 			</form>
 				<table>
 					<tr>
@@ -32,7 +36,7 @@
 							$sql_register = mysqli_query($conection, "SELECT COUNT(*) AS total_registro FROM usuario WHERE estatus = 1");
 							$result_register = mysqli_fetch_array($sql_register);
 							$total_registro = $result_register['total_registro'];
-							$por_pagina = 9;
+							$por_pagina = 5;
 							if(empty($_GET['pagina'])){
 								$pagina = 1;
 							}else{
@@ -40,7 +44,8 @@
 							}
 								$desde = ($pagina - 1) * $por_pagina;
 								$total_paginas = ceil($total_registro / $por_pagina); //CEIL SIRVE PARA REDONDEAR
-									$query = mysqli_query($conection, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE estatus = 1 ORDER BY idusuario ASC LIMIT $desde, $por_pagina");
+									$query = mysqli_query($conection, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE estatus = 1 ORDER BY u.idusuario ASC LIMIT $desde, $por_pagina");
+										mysqli_close($conection);
 											$result = mysqli_num_rows($query);
 												if($result	> 0){
 													while (	$data = mysqli_fetch_array($query)) {
@@ -52,13 +57,13 @@
 									<td><?php echo $data["usuario"] ?></td>
 									<td><?php echo $data["rol"] ?></td>
 									<td>
-										<a class = "link_edit" href="editar_usuario.php?id=<?php echo $data["idusuario"] ?>">Editar</a>
+										<a class = "link_edit" href="editar_usuario.php?id=<?php echo $data["idusuario"] ?>"><i class="far fa-edit"></i> Editar</a>
 										<?php
 											if($data["idusuario"] != 1 ){
 											
 										?>
 										|
-										<a class = "link_delete" href="eliminar_usuario.php?id=<?php echo $data["idusuario"] ?>">Eliminar</a>
+										<a class = "link_delete" href="eliminar_usuario.php?id=<?php echo $data["idusuario"] ?>"><i class="fa fa-trash" aria-hidden="true"></i> Eliminar</a>
 										<?php
 											}
 										?>
@@ -74,8 +79,8 @@
 								<?php 
 									if($pagina != 1){
 								?>	
-								<li><a href="?pagina=<?php echo 1; ?>">|<</a></li>
-								<li><a href="?pagina=<?php echo $pagina - 1; ?>"><<</a></li>
+								<li><a href="?pagina=<?php echo 1; ?>"><i class="fa fa-step-backward" aria-hidden="true"></i></a></li>
+								<li><a href="?pagina=<?php echo $pagina - 1; ?>"><i class="fa fa-backward" aria-hidden="true"></i></a></li>
 								<?php
 									}
 									for ($i=1; $i <= $total_paginas; $i++) { 
@@ -87,8 +92,8 @@
 									}
 									if($pagina != $total_paginas){
 								?>
-								<li><a href="?pagina=<?php echo $pagina + 1; ?>">>></a></li>
-								<li><a href="?pagina=<?php echo $total_paginas; ?>">>|</a></li>
+								<li><a href="?pagina=<?php echo $pagina + 1; ?>"><i class="fa fa-forward" aria-hidden="true"></i></a></li>
+								<li><a href="?pagina=<?php echo $total_paginas; ?>"><i class="fa fa-step-forward" aria-hidden="true"></i></a></li>
 								<?php
 									}
 								?>
