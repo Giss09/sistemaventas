a677ab97c541574b8fa79748eb70c8b34bcb8390
Giss09 <98498457+Giss09@users.noreply.github.com>
2022-03-15 20:47:44 -0500

    Footer realizado por Darlyn

--- sistema/include/footer.php
+++ sistema/include/footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="contenedor">
     <div class="copyright">
-      <p>Proyecto Realizado por Darlyn y Gissella</p>
+      <p>Proyecto Realizado por Darlyn Changuan y Gissella Urgilés</p>
       </div>
     </div>
 </footer>
