e7ef774dfce92a752e859422331605c430d97170
Giss09 <98498457+Giss09@users.noreply.github.com>
2022-03-15 20:46:27 -0500

    Update footer.php

--- sistema/include/footer.php
+++ sistema/include/footer.php
@@ -1,1 +1,7 @@
-<p>Proyecto Realizado por Darlyn y Gissella</p>
+<footer>
+  <div class="contenedor">
+    <div class="copyright">
+      <p>Proyecto Realizado por Darlyn y Gissella</p>
+      </div>
+    </div>
+</footer>
